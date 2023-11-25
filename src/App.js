5ee47ddc5698d507d8a2b5f39fcdd236e406b386
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2023-11-25 17:20:45 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ export const config = {
     clientId: '6bea6dc5-5661-4e66-adfd-ffcc127ef544', // Replace with your Azure AD application's client ID
     authority:
       'https://login.microsoftonline.com/ffd3cb73-11ea-4c26-8855-6a8f5d2fd6e5',
-    postLogoutRedirectUri: 'https://garagepi.site/loginRedirect',
-    redirectUri: 'https://garagepi.site/loginRedirect',
+    postLogoutRedirectUri: 'https://www.garagepi.site/loginRedirect',
+    redirectUri: 'https://www.garagepi.site/loginRedirect',
   },
   cache: {
     cacheLocation: 'localStorage',
