db500ef3f5d41d3142017d268680c2c6b4aff1ee
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2024-05-28 16:30:02 +1200

    Update GarageDoorSkeleton.js

--- src/GarageDoorSkeleton.js
+++ src/GarageDoorSkeleton.js
@@ -3,12 +3,16 @@ import { Skeleton, Grid, Paper } from '@mui/material';
 
 const GarageDoorSkeleton = ({ count }) => {
   return (
-    <Grid container spacing={2} alignItems="center" justifyContent="center">
+    <Grid
+      container
+      spacing={2}
+      sx={{ mt: 1 }}
+      alignItems="center"
+      justifyContent="center"
+    >
       {Array.from({ length: count }).map((_, index) => (
         <Grid item key={index}>
-          <Paper>
-            <Skeleton variant="rectangular" width={400} height={130} />
-          </Paper>
+          <Skeleton variant="rounded" width={400} height={136} />
         </Grid>
       ))}
     </Grid>
